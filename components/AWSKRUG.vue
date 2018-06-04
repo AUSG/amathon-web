1d2dad50e3ef56af695806e8835c1c6312aafcdd
Exubient <henrynicholas09@gmail.com>
2018-06-04 22:56:24 +0900

    add hackathon info, need pics

--- components/AWSKRUG.vue
+++ components/AWSKRUG.vue
@@ -2,20 +2,26 @@
   <div class="awskrug">
     <div class="awskrug-photo-1"></div>
     <div class="awskrug-introduction">
-      <h4>AWSKRUG</h4>
-      <p>최신 IT기술과 클라우드에 대한 이해를 바탕으로 AWS를 학습하고 전파하는 대학생 그룹</p>
+      <h4>AWSKRUG : AWS KoRea User Group</h4>
+      <p>AWSKRUG는 2012년 시작된 아마존웹서비스 한국 사용자모임으로서, AWS 클라우드를 처음 배우는 분들로 부터 고급 개발자까지 정보 교류를 위해 만들었습니다. 각종 반기 및 월간 세미나, 지역별 분야별 소모임 그리고 글로벌 유저그룹 연계 활동 등 다양한 프로젝트를 수행하고 있으며, 자세한 내용은 아래 소개 자료를 참고하시기 바랍니다.</p>
+
+      <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse"><ul id="menu-top" class="nav navbar-nav navbar-right"><li id="menu-item-4356" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4356"><a title="페이스북 그룹" href="/fb-post">페이스북 그룹</a></li>
+      <li id="menu-item-9419" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9419"><a title="Slack 채널" href="http://www.awskr.org/slack/">Slack 채널</a></li>
+      <li id="menu-item-9423" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9423"><a title="소모임 안내" href="http://www.awskr.org/groups/">소모임 안내</a></li>
+      <li id="menu-item-9581" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9581"><a title="행사 안내" href="http://www.awskr.org/events/">행사 안내</a></li>
+      <li id="menu-item-4188" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-3998 current_page_item menu-item-4188 active"><a title="커뮤니티 소개" href="http://www.awskr.org/about/">커뮤니티 소개</a></li>
+      </ul></div>
+
+
     </div>
     <div class="awskrug-photo-2"></div>
     <div class="awskrug-activities">
       <h4>Activities</h4>
       <p>
-        awskrug 멤버들의 AWS 학습을 위한 교육 세션<br />
-        AWS 학습의 응용을 위한 팀 별 프로젝트 진행<br />
-        AWS 전파를 위한 세미나 주최<br />
-        외부 대학생 대상 AWS교육<br />
-        AWS와 AWSKRUG의 공식행사 서포트<br />
-        AWSKRUG의 #awskrug 채널 및 모임 운영<br />
-        해외유저그룹 행사 및 AWS 공식행사 참여
+        국내 AWS 학습 및 정보 공유 모임<br />
+        AWS Korea와의 협력활동<br />
+        AWS re:invent 등의 행사활동<br />
+        해외 AWS 모임과의 교류활동<br />
       </p>
     </div>
     <div class="awskrug-activities-background"></div>
--- components/Hackathon.vue
+++ components/Hackathon.vue
@@ -1,3 +1,149 @@
 <template>
-  <div class="hackathon"></div>
+  <div class="hackathon">
+  
+  <div class="hackathon-title">
+  <h3>AMATHON : AMAteur hackaTHON</h3> 
+  </div>
+
+  <div class="hackathon-aim">
+  <h3>Amathon(아마톤)의 정의 및 목표</h3>
+  <p>해커톤이란 해킹(Hacking)과 마라톤(Marathon)의 합성어로, 
+  기획자 디자이너 개발자가 한 팀을 이뤄서 마치 마라톤을 하는 것처럼 하룻동안 쉬지 않고 
+  아이디어를 프로그래밍 과정을 통하여 결과물을 만들어 내는 활동 입니다. 
+  하지만 한국에서는 공모전의 일종으로 수상을 하기 위한 하나의 이벤트로서 많이 변질되어 왔습니다. 
+  AUSG는 해커톤이 프로페셔널한 사람들이 모여서 수상을 하기 위한 경쟁의 도구가 아니라 초보자라도 
+  클라우 드에 대해서 편안하게 접할 수 있고, 현업에서 사용하는 도구와 기술들을 만나보며 
+  학업과 현업의 거리를 줄이 려는 행사를 위하여 아마톤를 기획하게 되었습니다.</p>
+  </div>
+
+  <div class="hackathon-intro">
+  <h3>Amathon 행사정보</h3>
+  <pre>진행일자 : 18년 7월 20(금) ~ 7월 21(토)
+주최 : AWSKRUG - AUSG (팀페이지 링크)
+장소 : 마루 180
+시간 : 10:00 (금) ~ 12:00(토)
+목표 참석 인원 : 60명
+후원 : Megazone
+스타트업 : 
+</pre>
+  </div>
+
+
+  <div class="hackathon-schedule">
+  <table>
+  <thead>
+  <tr>
+  <th scope="col" colspan="2">구분</th>
+  <th scope="col">시간</th>
+  <th scope="col">내용</th>
+  <th scope="col">장소</th>
+  </tr>
+  </thead>
+  
+  <tr id="1">
+  <th scope="row" rowspan="13">토</th>
+  <td rowspan="2">입장 및 자리배치</td>
+  <td rowspan="2">09:00 - 09:30</td>
+  <td>▸ 기념품(텀블러, 티셔츠, 스티커, USB) 및 이름표 수령</td>
+  <td rowspan="7">대회의장</td>
+  </tr>
+
+  <tr id="2">
+  <td>▸ 팀별 자리배치</td>
+  </tr>
+ 
+  <tr id="3">
+  <td rowspan="2">개회식</td>
+  <td rowspan="2">09:00 -10:00</td>
+  <td> ▸ 키노트
+- AWS Tech Evangelist 윤석찬 및 후원사
+  </td>
+  </tr>
+
+  <tr id="4">
+  <td>▸ 오프닝
+- AUSG 남궁선
+  </td>
+  </tr>
+
+  <tr id="5">
+  <td>팀빌딩</td>
+  <td>10:00 - 10:30</td>
+  <td>▸ 아이스브레이킹</td>
+  </tr>
+
+  <tr id="6">
+  <td>교육세션</td>
+  <td>10:30 - 11:30</td>
+  <td>▸ 아이디에이션 및 디자인씽킹 방법론 
+  <br>▸ 웹/앱 프로토타이핑 방법론
+  </td>
+  </tr>
+
+  <tr id="7">
+  <td>중식</td>
+  <td>11:30 - 12:00</td>
+  <td>▸ 점심 도시락 배부, 자유 식사</td>
+  </tr>
+
+  <tr id="8">
+  <td rowspan="3">인프라별 선택교육세션</td>
+  <td rowspan="3">12:00 - 13:00</td>
+  <td>▸ EC2 + RDS를 이용한 Django Web Aplication Server</td>
+  <td rowspan="3">세미나실</td>
+  </tr>
+
+  <tr id="9">
+  <td>▸ S3 + DynamoDB를 이용한 Single Page Web Application</td>
+  </tr>
+
+  <tr id="10">
+  <td>▸ Lamda +RDS를 이용한 RESTful API</td>
+  </tr>
+
+  <tr id="11">
+  <td>개발</td>
+  <td>13:00 - 18:30</td>
+  <td>▸ 팀별 프로젝트 진행 및 후원사 개발자 멘토링</td>
+  <td rowspan="6">대회의장</td>
+  </tr>
+
+  <tr id="12">
+  <td>석식 및 이벤트</td>
+  <td>18:30 - 19:30</td>
+  <td>▸ 도시락 배부 및 레크리에이션</td>
+  </tr>
+
+  <tr id="13">
+  <td>야식 배부</td>
+  <td>24:00</td>
+  <td>▸ 피자 & 치킨</td>
+  </tr>
+
+  <tr id="14">
+  <th rowspan="3">일</th>
+  <td>개발</td>
+  <td>24:00 - 09:00</td>
+  <td>▸ 간단한 아침식사 제공</td>
+  </tr>
+
+  <tr id="15">
+  <td>발표 준비</td>
+  <td>08:00 - 09:00</td>
+  <td>▸ 결과물 제출, 발표 준비</td>
+  </tr>
+
+  <tr id="16">
+  <td>발표 및 시상</td>
+  <td>09:00 - 10:00</td>
+  <td>▸ 결과물 공유 및 팀별 회고</td>
+  </tr>
+  </table>
+  </div>
+
+  <div class="ausg-activities-background"></div>
+
+  </div>
+
+  
 </template>
